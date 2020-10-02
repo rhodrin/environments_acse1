68f36b73e5f684db6603d274b968dfa1bdeba8bd
rhodrin <rhodri.nelson@gmail.com>
2020-10-02 10:27:06 +0100

    v3.

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
-__all__ = ['rand_array', 'smooth_image']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
 
 def rand_array(shape):
@@ -10,3 +10,6 @@ def rand_array(shape):
 
 def smooth_image(a, sigma=1):
     return gaussian_filter(a, sigma=sigma)
+
+def my_mat_solve(A, b):
+    return A.inv()*b
